b4265bb20040480992a0ade2545056298fa860d2
biruk1122 <birukfikadu29@gmail.com>
2025-08-16 20:57:53 +0300

    generating the header part

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react"
+import Search from "./components/Search"
+
 function App() {
-  return <div>App</div>
+  const [searchTerm, setSearchTerm] = useState("")
+
+  return (
+    <main className="p-8 bg-gray-950 min-h-screen flex items-center justify-center">
+      <div className="pattern" />
+
+      <div>
+        <header>
+          <img
+            src="./movie-image.jpg"
+            alt="Movie image"
+            className="block mx-auto w-[300px] h-[400px] mix-blend-screen opacity-60 "
+          />
+          <h1 className="text-4xl md:text-5xl font-bold text-gray-100 leading-snug text-center">
+            Find{" "}
+            <span className="bg-gradient-to-r from-pink-500 via-red-500 to=yellow-500 bg-clip-text text-transparent">
+              Movies
+            </span>{" "}
+            You'll Enjoy Without the Hassle
+          </h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+    </main>
+  )
 }
 
 export default App
---
+++ src/components/Search.jsx
@@ -0,0 +1,5 @@
+function Search() {
+  return <div className="text-white text-3xl">Search</div>
+}
+
+export default Search
